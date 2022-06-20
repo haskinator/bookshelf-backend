029e84f70ac4c31f094cdf9d8c5a849f1cc771ca
Zuzana Beňová <zuzanabenova@Zuzanas-MacBook-Air.local>
2022-06-20 11:29:23 +0200

    postgres added

--- knexfile.js
+++ knexfile.js
@@ -15,4 +15,18 @@ module.exports = {
     }
   },
 
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool:{
+      min:2,
+      max:10
+    },
+    migrations:{
+      tablename: 'knex_migrations',
+      directory: './migrations'
+    }
+
+  }
+
 };
