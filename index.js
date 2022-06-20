1e8841e84e93ed3f908d34c3354b8caa619f1dd2
Zuzana Beňová <zuzanabenova@Zuzanas-MacBook-Air.local>
2022-06-20 12:05:11 +0200

    cors added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000 ;
+const cors = require('cors');
+app.use(cors({origin:'*'}))
 app.use(express.json());
 
 
