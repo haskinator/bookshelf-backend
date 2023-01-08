2dbd3a1c9f423d4d93a579762360b36ae522de5b
haskinator <zuzikbenova@gmail.com>
2023-01-08 19:05:12 +0100

    wip

--- knexfile.js
+++ knexfile.js
@@ -1,32 +1,29 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/bookshelf.sqlite3'
+      filename: "./data/bookshelf.sqlite3",
     },
-    useNullAsDefault:true
+    useNullAsDefault: true,
   },
-  pool:{
-    afterCreate:(conn,done)=>{
-      conn.run("PRAGMA foreign_keys=ON",done)
-    }
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys=ON", done);
+    },
   },
 
   production: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
-    pool:{
-      min:2,
-      max:10
+    // pool:{
+    //   min:2,
+    //   max:10
+    // },
+    migrations: {
+      tablename: "knex_migrations",
+      directory: "./migrations",
     },
-    migrations:{
-      tablename: 'knex_migrations',
-      directory: './migrations'
-    }
-
-  }
-
+  },
 };
